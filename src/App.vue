740e318628d111ea44728097493607b77bdbb56f
dcancho <102440160+dcancho@users.noreply.github.com>
2023-11-30 20:14:07 +0000

    config: set up project and dependencies.

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NavSidebar from './components/NavSidebar.vue';
+</script>
+
+<template>
+  <NavSidebar />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+//import router from './router/index';
+import 'primevue/resources/themes/fluent-light/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(PrimeVue, { ripple: true });
+//app.use(router);
+
+app.mount('#app');
