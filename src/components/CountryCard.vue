bfce44e651ac99cb4ea5babdaa7779fc48caec14
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2023-12-02 23:21:26 -0500

    hotfix: fix cards max size update title and favicon

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -73,7 +73,7 @@ onMounted(() => {
     border-radius: 0.5rem;
     box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
     background-color: white;
-
+    max-height: 16rem;
 }
 
 .loading-icon {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,16 @@
 <template>
-    <p>Hello from About page</p>
+    <div class="wrapper">
+        <h1>Made by Diego Cancho Coila</h1>
+    </div>
 </template>
+
+<style scoped>
+.wrapper {
+    height: 100vh;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+}
+</style>
